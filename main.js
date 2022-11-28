3310b4cf0f1512136ed04be7f5d2a78502c931b4
zaha1223 <muhammadzohidbahodirov04@gmail.com>
2022-11-28 21:40:20 +0500

    start

---
+++ main.js
@@ -0,0 +1,38 @@
+let inptCity = document.querySelector(".cityInput");
+let form = document.querySelector(".form");
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a3e783baa2msh384e71d79fda43fp128491jsnb123f431e574",
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+  },
+};
+
+let elDiv = document.querySelector(".location__weather");
+
+let renderWeather = (data) => {
+  console.log(data);
+  elDiv.innerHTML = `
+  <h4><strong>Region</strong> ${data.location.region}</h4>
+      <h5><strong>Country</strong> ${data.location.country}</h5>
+      <p><strong>Weather</strong> ${data.current.condition.text}</p>
+      <img src="${data.current.condition.icon}" alt="img" width="50" height="50" />
+      <p> ${data.current.temp_c}C</p>
+	  <span><strong>Real time</strong> ${data.location.localtime}</span>`;
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let inputVal = inptCity.value;
+
+  elDiv.classList.add("location__weather1");
+
+  fetch(
+    `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${inputVal}&days=3`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => renderWeather(response))
+    .catch((err) => console.error(err));
+});
